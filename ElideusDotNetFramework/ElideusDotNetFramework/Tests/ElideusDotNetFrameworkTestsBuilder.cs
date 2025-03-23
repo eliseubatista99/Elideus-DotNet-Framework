76819faf75ea4bb28c555151754c321489614760
Eliseu Batista <eliseubatista99@outlook.com>
2025-03-23 20:08:51 +0000

    Backup

--- ElideusDotNetFramework/ElideusDotNetFramework/Tests/ElideusDotNetFrameworkTestsBuilder.cs
+++ ElideusDotNetFramework/ElideusDotNetFramework/Tests/ElideusDotNetFrameworkTestsBuilder.cs
@@ -48,5 +48,6 @@ namespace ElideusDotNetFramework.Tests
                 _initialized = true;
             }
         }
+
     }
 }
---
+++ ElideusDotNetFramework/ElideusDotNetFramework/Tests/OperationTest.cs
@@ -0,0 +1,26 @@
+﻿using ElideusDotNetFramework.Operations;
+using ElideusDotNetFramework.Operations.Contracts;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ElideusDotNetFramework.Tests;
+
+public class OperationTest<TOperation, TIn, TOut> where TOperation : BaseOperation<TIn, TOut> where TIn : OperationInput where TOut : OperationOutput
+{
+    protected TOperation? operationToTest;
+
+    protected virtual void Setup()
+    {
+
+    }
+
+    protected virtual async Task<TOut> SimulateCall(TOperation operation, TIn input)
+    {
+        var callResult = await operation!.Call(input).ConfigureAwait(false);
+
+        var result = (OperationHttpResult) callResult;
+
+        return (TOut) result.Output!;
+    }
+}
