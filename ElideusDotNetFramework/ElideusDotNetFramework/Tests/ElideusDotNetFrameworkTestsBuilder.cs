7d6512888f615bedb5ffed84b6c74ada401c5a8c
Eliseu Batista <eliseubatista99@outlook.com>
2025-03-23 23:27:59 +0000

    Finished tests

--- ElideusDotNetFramework/ElideusDotNetFramework/Tests/ElideusDotNetFrameworkTestsBuilder.cs
+++ ElideusDotNetFramework/ElideusDotNetFramework/Tests/ElideusDotNetFrameworkTestsBuilder.cs
@@ -12,8 +12,6 @@ namespace ElideusDotNetFramework.Tests
 
         public ElideusDotNetFrameworkTestsBuilder()
         {
-            ApplicationContextMock = new ApplicationContextMock().Mock();
-
             Initialize();
         }
 
