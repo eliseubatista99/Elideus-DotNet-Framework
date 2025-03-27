0a6ed98a67bb7e2c49d00c3e99391a877c29c0e3
Eliseu Batista <eliseubatista99@outlook.com>
2025-03-27 22:39:07 +0000

    Backup

--- ElideusDotNetFramework/ElideusDotNetFramework.ExternalServices/Providers/ExternalServiceProvider.cs
+++ ElideusDotNetFramework/ElideusDotNetFramework.ExternalServices/Providers/ExternalServiceProvider.cs
@@ -10,7 +10,7 @@ namespace ElideusDotNetFramework.ExternalServices
     public class ExternalServiceProvider : IExternalServiceProvider
     {
         protected IApplicationContext applicationContext { get; set; }
-        protected static HttpClient httpClient { get; set; } = new HttpClient();
+        protected static HttpClient httpClient { get; set; }
 
         public ExternalServiceProvider(IApplicationContext _applicationContext)
         {
@@ -24,20 +24,17 @@ namespace ElideusDotNetFramework.ExternalServices
 
         protected virtual async Task<TOut> CallExternalPostOperation<TIn, TOut>(string endpoint, TIn input) where TIn : OperationInput where TOut : OperationOutput
         {
-            httpClient.BaseAddress = new Uri(GetServiceUrl());
-            httpClient.DefaultRequestHeaders
-                  .Accept
-                  .Add(new MediaTypeWithQualityHeaderValue("application/json"));//ACCEPT header
+            Uri requestUri = new Uri($"{GetServiceUrl()}/{endpoint}");
 
-            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, endpoint);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, requestUri);
 
+            request.Headers.Accept.Clear();
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             request.Content = new StringContent(JsonSerializer.Serialize(input),
                                                 Encoding.UTF8,
                                                 "application/json");//CONTENT-TYPE header
 
-
-
-            var response = await httpClient.SendAsync(request);
+            var response = await httpClient.SendAsync(request, CancellationToken.None);
             var operationResult = await response.Content.ReadFromJsonAsync<TOut>();
 
             return operationResult!;
